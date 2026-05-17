8e9458f9498261a320497d1ac3c5e383e7c3fdd9
singhsona27 <72622691+singhsona27@users.noreply.github.com>
2026-05-17 10:34:17 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:40,fontFamily:"Arial"}}>
+      <h1>Solar Solution Calculator</h1>
+      <p>Your web app is running successfully.</p>
+    </main>
+  );
+}
