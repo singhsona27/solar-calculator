2d1470d2fe34d65e98e156a6f202d7fc8f52a9eb
singhsona27 <72622691+singhsona27@users.noreply.github.com>
2026-05-17 10:33:40 +0100

    Add RootLayout and metadata for Solar Calculator

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Solar Solution Calculator",
+  description: "Solar Calculator"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
